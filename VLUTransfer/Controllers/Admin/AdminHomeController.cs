ad8c544a2fc82217fa6954bef89ea56e7f2f6d9b
Your Name <you@example.com>
2022-05-03 01:30:46 +0700

    first commit

---
+++ VLUTransfer/Controllers/Admin/AdminHomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumCoderASP.Controllers.Admin
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            string result = (string)Session["Admin"];
+            if (string.IsNullOrEmpty(result))
+            {              
+                return RedirectToAction("Index", "AdminLogin");
+            }
+            else
+            {
+                return View();
+            }
+              
+        }
+    }
+}
